e0d2bbeebb1a2e3e6cbf5349a2f03acbf44b4c47
Fernando Merlo <ctrbts.dev@gmail.com>
2022-10-17 22:06:18 -0300

    first upload

---
+++ 00.docker-template/project/src/index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Hola mundo desde docker</title>
+        <style>
+            .center {
+                text-align: center;
+            }
+
+            .ital{
+                text-decoration: none;
+                font-style: italic;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="center">
+            <h3>Hola mundo!</h3>
+            <h5>Correindo desde docker!</h5>
+            <br><br>
+            <a class="ital" href="./info.php">PHP info</a>
+        </div>
+    </body>
+</html>
